c7156a8705105ce5569060c403bf04bb257f63b1
Erika <al214717@alumnos.uacj.mx>
2025-11-17 20:26:35 -0700

    Detalles en shop

--- Frontend_WS/src/app/App.jsx
+++ Frontend_WS/src/app/App.jsx
@@ -1,29 +1,17 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-<<<<<<< HEAD
-import Home from "../features/Home";
-import Login from "../features/Login";
-import Register from "../features/Register";
-import VerifyEmail from "../features/VerifyEmail";
-/*Rutas de nuevas views Y rutas en espera*/ 
-import Footer from "../features/Footer";
-import Shop from "../features/Shop";
-import ShoppingCar from "../features/ShoppingCar";
-import Navbar from "../features/Navbar";
-import ProductDetail from "../features/ProductDetail";
-import Publish from "../features/Publish";
-import Terms from "../features/Terms";
-import About from "../features/AboutUS";
-{/*import Category from "../features/Category";
-=======
 
 import Home from "../features/Home.jsx";
 import VerifyEmail from "../features/VerifyEmail.jsx";
 
 import Footer from "../features/Footer.jsx";
+import Terms from "../features/Terms.jsx";
+import About from "../features/AboutUS.jsx";
+
+import Navbar from "../features/Navbar.jsx";
+
 import Shop from "../features/Shop.jsx";
 import ShoppingCar from "../features/ShoppingCar.jsx";
-import Navbar from "../features/Navbar.jsx";
 import ProductDetail from "../features/ProductDetail.jsx";
 
 import { LoginSignup } from "../features/login-signup.jsx";
@@ -39,7 +27,6 @@ import PublishProduct from "../features/PublishProduct.jsx";
 import MyListings from "../features/MyListings.jsx";
 import CategoryPage from '../features/CategoryPage';
 
->>>>>>> 1580d40b68f8865583f41de655ba3cc037cace3b
 import Nursery from "../features/Nursery";
 import Exchange from "../features/Exchange";
 
@@ -52,31 +39,8 @@ const mockSellers = [];
 export default function App() {
   return (
     <Router>
-<<<<<<< HEAD
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />              {/* Página principal */}
-        <Route path="/shop" element={<Shop />} /> 
-        <Route path="/shoppingcar" element={<ShoppingCar />}/>
-        <Route path="/productdetail" element={<ProductDetail />}/>
-        <Route path="/login" element={<Login />} />      {/* Iniciar sesión */}
-        <Route path="/register" element={<Register />} />  {/* Registro */}
-        <Route path="/verify-email" element={<VerifyEmail />} /> {/* Verificación */}
-        <Route path="/publish" element={<Publish />} />
-        <Route path="/terms" element={<Terms />} />
-         <Route path="/about" element={<About />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-        {/* RUTAS EN ESPERA
-        <Route path="/nursery" element={<Nursery />} /> 
-        <Route path="/exchange" element={<Exchange />} /> 
-        */}
-      </Routes>
-      <Footer />
-    </Router>
-=======
       <ProductsProvider>
         <Navbar />
->>>>>>> 1580d40b68f8865583f41de655ba3cc037cace3b
 
         <Routes>
           <Route path="/" element={<Home />} />
@@ -122,11 +86,16 @@ export default function App() {
 
           <Route path="/nursery" element={<Nursery />} /> 
           <Route path="/exchange" element={<Exchange />} /> 
+        {/* Footer */}
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/about" element={<About />} />
+
 
           {/* 404 */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
 
+       
         <Footer />
       </ProductsProvider>
     </Router>
